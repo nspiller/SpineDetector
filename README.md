7f78e3f55437c476f4a0041344335941823e48e6
Ryohei Yasuda <yasudar@mpfi.org>
2023-05-18 17:24:45 -0400

    ReadMe file update

--- README.md
+++ README.md
@@ -12,13 +12,13 @@ The following example can be used for installation.
 
 Tested in python 3.11.3. 
 
-'''bash
+```bash
 git clone https://github.com/ryoheiyasuda/SpineDetector.git
 cd spinedetect
 python(or python3) -m venv venv
 ./venv/Scripts/activate
 pip install -r requirements.txt
-'''
+```
 
 # Usage
 The following example demonstrates the use of the code for detecting spines from images in the "sample" folder.
--- Train.py
+++ Train.py
@@ -21,7 +21,7 @@ file_path = Path(file).parent.resolve()
 yolo_path = os.path.join(file_path, 'yolov5')
 yolo_git = 'https://github.com/ultralytics/yolov5.git'
 if not os.path.exists(yolo_path):
-    Repo.clone_from(yolo_git, file_path)
+    Repo.clone_from(yolo_git, yolo_path)
     import pip
     req_file = os.path.join(yolo_path, 'requirements.txt')
     pip.main(['install', '-r', req_file])    
@@ -51,22 +51,22 @@ torch.cuda.empty_cache()
 if yolo_path not in sys.path:
     sys.path.insert(0, yolo_path)
 
-from train import run as run_train
+import train
 
-yolo_thrash = yolo_path + "\\"
-run_train(weights=yolo_thrash+'yolov5l.pt', data=yolo_thrash+'data.yaml', hyp=yolo_thrash+'data\hyps\hyp.scratch-low.yaml', epochs=300, batch_size=32, imgsz=416, rect=False, resume=False, nosave=False, noval=False, noautoanchor=False, noplots=False, evolve=None, cache='ram', image_weights=False, multi_scale=False, single_cls=False, optimizer='SGD', sync_bn=False, workers=8, project=yolo_thrash+'models/YoloDendritic', name='SpineDetection', exist_ok=False, quad=False, cos_lr=False, label_smoothing=0.0, patience=100, freeze=[0,], save_period=-1, seed=0, local_rank=-1, entity=None, upload_dataset=False, bbox_interval=-1,artifact_alias='latest')
+yolo_path1 = yolo_path + "\\"
+train.run(weights=yolo_path1+'yolov5l.pt', data=yolo_path1+'data.yaml', hyp=yolo_path1+'data\hyps\hyp.scratch-low.yaml', epochs=300, batch_size=32, imgsz=416, rect=False, resume=False, nosave=False, noval=False, noautoanchor=False, noplots=False, evolve=None, cache='ram', image_weights=False, multi_scale=False, single_cls=False, optimizer='SGD', sync_bn=False, workers=8, project=yolo_path1+'models/YoloDendritic', name='SpineDetection', exist_ok=False, quad=False, cos_lr=False, label_smoothing=0.0, patience=100, freeze=[0,], save_period=-1, seed=0, local_rank=-1, entity=None, upload_dataset=False, bbox_interval=-1,artifact_alias='latest')
 
 '''
 Alternatively, you can just change directory to yolo and run in the system command:
-python train.py  --img 416 --batch 32 --epochs 300 --data data.yaml --weights yolov5l.pt --project "models/YoloDendritic" --name SpineDetection --cache
+python train.py  --imgsz 416 --batch 32 --epochs 300 --data data.yaml --weights yolov5l.pt --project "models/YoloDendritic" --name SpineDetection --cache
 '''
 
 
 #%%
-from val import run as run_val
-run_val(weight=yolo_thrash+'weights/best.pt', data=yolo_thrash+'data.yaml', imgsz=416, project='YoloDendritic', name='Valid')
+import val
+val.run(weight=yolo_path1+'weights/best.pt', data=yolo_path1+'data.yaml', imgsz=416, project='YoloDendritic', name='Valid')
 
 '''
 You can also run in the system command:
-python val.py --weights "weights/best.pt" --data data.yaml --img 416 --project "YoloDendritic" --name Valid
+python val.py --weights "weights/best.pt" --data data.yaml --imgsz 416 --project "YoloDendritic" --name Valid
 '''
